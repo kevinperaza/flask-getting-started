4650159882d3ec1f530612f2be86a6920e52fbca
Kevin <kevin.peraza@live.com>
2021-04-21 18:45:17 -0600

    Create card

--- flash_cards.py
+++ flash_cards.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import Flask, render_template, abort, jsonify, request, redirect
+from flask.globals import request
+from flask.helpers import url_for
+from model import db, save_db
 
 app = Flask(__name__)
 
@@ -17,6 +19,16 @@ def card_view(index):
         return render_template("card.html", card=card, index=index, max_index=len(db) - 1 )
     except IndexError:
         return abort(404)
+
+@app.route("/cards/create", methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        # process data
+        db.append(dict(request.form.to_dict(), **{"index": len(db) -1}))
+        save_db()
+        return redirect(url_for("card_view", index=len(db) -1))
+    else:
+        return render_template("create_card.html")
 
 # API
 
--- model.py
+++ model.py
@@ -1,7 +1,15 @@
 import json
+
+
+endpoint = "flashcards_db.json"
 # mock db
 def load_data():
-    with open("flashcards_db.json") as f:
+    with open(endpoint) as f:
         return json.load(f)
+
+def save_db():
+    with open(endpoint, 'w') as f:
+        return json.dump(db, f)
+
 
 db = load_data()
